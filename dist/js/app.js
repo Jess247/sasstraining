4fe16c9d2dafcaf6e543948250e878a2e8abe707
Jess247 <jesss8692@hotmail.com>
2021-04-06 10:15:12 +0200

    update javascript
    change theme on btn click

---
+++ dist/js/app.js
@@ -0,0 +1,11 @@
+const themeBtn = document.querySelector('.btn').addEventListener('click', function() {
+   let theme =  document.querySelector('body');
+
+   if(theme.classList.contains('dark')) {
+       theme.classList.remove('dark');
+       theme.classList.add('light');
+   } else {
+    theme.classList.remove('light');
+    theme.classList.add('dark');
+   }
+});
